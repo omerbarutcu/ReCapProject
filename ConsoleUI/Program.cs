64dcee2c8191354b009fe7c858e1921c24b81d94
ömer barutçu <omerbrtc@hotmail.com>
2023-12-25 22:12:54 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,55 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.CarId = 9;
+            car1.BrandId = 10;
+            car1.ColorId = 11;
+            car1.Description = "Doğan SLX";
+            car1.DailyPrice = 150;
+            car1.ModelYear = 1998;
+
+            Car car2 = new Car();
+            car2.CarId = 1;
+            car2.BrandId = 7;
+            car2.ColorId = 6;
+            car2.Description = "Mercedes E200";
+            car2.DailyPrice = 130;
+            car2.ModelYear = 1995;
+
+            Console.WriteLine("--------GetAll-------");
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine("--------GetById-------");
+
+            Console.WriteLine(carManager.GetById(1).Description);
+
+            Console.WriteLine("--------Add-------");
+            carManager.Add(car1);
+            Console.WriteLine(carManager.GetById(9).Description);
+
+            Console.WriteLine("--------Update-------");
+            carManager.Update(car2);
+            Console.WriteLine(carManager.GetById(1).ModelYear + " model " + carManager.GetById(1).Description + " fiyatı : " + carManager.GetById(1).DailyPrice);
+
+            Console.WriteLine("--------Delete-------");
+
+            carManager.Delete(carManager.GetById(3));
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,ModelYear=2018,DailyPrice=300,Description="Ford Fiesta"},
+                new Car{CarId=2,BrandId=1,ColorId=2,ModelYear=2021,DailyPrice=550,Description="Ford Focus"},
+                new Car{CarId=3,BrandId=2,ColorId=3,ModelYear=2022,DailyPrice=500,Description="Citroen C3"},
+                new Car{CarId=4,BrandId=3,ColorId=2,ModelYear=2023,DailyPrice=600,Description="Renault Clio"},
+                new Car{CarId=5,BrandId=3,ColorId=2,ModelYear=2022,DailyPrice=650,Description="Renault Megane"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.SingleOrDefault(c => c.CarId == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
